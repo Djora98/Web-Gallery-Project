2769ba783cde50c2beca3c9e03eb1d5322ebc06e
Djora98 <luka.djoric.3363@metropolitan.ac.rs>
2021-03-13 23:18:07 +0100

    Fifth Commit
    Resolving problem with duplicate images on server side

--- server-side/file-upload-server/index.js
+++ server-side/file-upload-server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const helpers = require('./helpers');
+const dupli = require('./duplicates');
 const app = express();
 const port = 3000;
 
@@ -16,18 +17,39 @@ const storage = multer.diskStorage({
     },
 
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.originalname);
     }
 });
 
+// app.post('/api/uploads', (req, res) => {
+//     let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('picture');
+
+//     upload(req, res, function(err){
+//         if(req.fileValidationError){
+//             return res.send(req.fileValidationError);
+//         }
+//         else if (!req.file){
+//             return res.send('Please select an image to upload');
+//         }
+//         else if(err instanceof multer.MulterError){
+//             return res.send(err);
+//         }
+//         else if(err){
+//             return res.send(err);
+//         }
+
+//         res.send({ message: 'Image uploaded successfully!'});
+//     });
+// });
+
 app.post('/api/uploads', (req, res) => {
-    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('picture');
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).array('pictures', 10);
 
     upload(req, res, function(err){
         if(req.fileValidationError){
             return res.send(req.fileValidationError);
         }
-        else if (!req.file){
+        else if (!req.files){
             return res.send('Please select an image to upload');
         }
         else if(err instanceof multer.MulterError){
@@ -37,8 +59,10 @@ app.post('/api/uploads', (req, res) => {
             return res.send(err);
         }
 
-        res.send({ message: 'Image uploaded successfully!'});
+        res.send({ message: 'Images uploaded successfully!'});
     });
+
+    setTimeout(dupli.generateRefMap, 5000);
 });
 
 app.get('/api/uploads', (req, res) => {
